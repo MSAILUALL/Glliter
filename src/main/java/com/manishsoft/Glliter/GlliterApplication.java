06444af83268d3196388c788db9089942ed3e1cc
Manish <Manish@Manish>
2020-05-18 21:20:36 +0530

    First

---
+++ src/main/java/com/manishsoft/Glliter/GlliterApplication.java
@@ -0,0 +1,13 @@
+package com.manishsoft.Glliter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlliterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlliterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manishsoft/Glliter/GlliterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manishsoft.Glliter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlliterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
