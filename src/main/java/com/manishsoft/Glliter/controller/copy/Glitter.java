88d65d7b485abe2b18fea804afab6e1ed4ce9c47
Manish <Manish@Manish>
2020-05-20 00:27:25 +0530

    @Valu

--- src/main/java/com/manishsoft/Glliter/controller/copy/Glitter.java
+++ src/main/java/com/manishsoft/Glliter/controller/copy/Glitter.java
@@ -1,18 +1,30 @@
 package com.manishsoft.Glliter.controller.copy;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Glitter {
-	@Value("${message}")
+	@Value("Static Value")
+	private String msg1;
+	@Value("${message:Welcome to default value}")
 	private String message;
+	@Value("${listMessage}")
+	private List<String> msg;
+	@Value("#{${mapvalue}}")
+	private Map<String, String> dbvalue;
 	
 	
 	@GetMapping("/gliter")
 	public String getGliter() {
-		return message;
+		dbvalue.get("connection");
+		System.out.println("======"+dbvalue.get("connection"));
+		return message+"-----"+msg+"-----"+msg1+"----"+dbvalue;
+		
 
 	}
 }
