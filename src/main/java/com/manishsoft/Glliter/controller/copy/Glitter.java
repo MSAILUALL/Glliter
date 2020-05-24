04fc8d5d068433c6352e1be7329f81ef6c00cc34
Manish <manish.kumar731@gmail.com>
2020-05-24 11:27:49 +0530

    final stag

--- src/main/java/com/manishsoft/Glliter/controller/copy/Glitter.java
+++ src/main/java/com/manishsoft/Glliter/controller/copy/Glitter.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.manishsoft.Glliter.Bo.GlliterEmployee;
 
 @RestController
+@RefreshScope
+//http://localhost:8080/actuator/refresh (POST Request)
 public class Glitter {
 	@Value("Static Value")
 	private String msg1;
